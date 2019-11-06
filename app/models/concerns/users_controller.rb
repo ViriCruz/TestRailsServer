b0febf83cb6897cac2ef85239069a27308d83dd8
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-11-06 10:53:18 -0700

    minor changes

--- app/models/concerns/users_controller.rb
+++ app/models/concerns/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy  ]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  
+
   def index
+    # @users = User.paginate(page: params[:page])
     @users = User.where(activated: true).paginate(page: params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
@@ -35,6 +44,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
+
     else
       render 'edit'
     end
@@ -47,11 +57,11 @@ class UsersController < ApplicationController
   end
 
   private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  # Before filters
 
     # Confirms a logged-in user.
     def logged_in_user
@@ -68,6 +78,7 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
+    # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
